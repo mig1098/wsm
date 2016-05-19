72e13fe5b7c98fe4937ed47b39057b249f8197e2
Denis Rosenkranz <tubezleb@gmail.com>
2016-05-18 22:54:43 -0400

    update wsm.sh

--- wsm.sh
+++ wsm.sh
@@ -2,6 +2,8 @@
 ##denisrosenkranz.com##
 ##Web Server Management script##
 
+WSM=/opt/wsm
+
 echo "#############################################"
 echo "########Web Server management Script#########"
 echo "#############################################"
@@ -43,12 +45,19 @@ case $choice in
 	chown -R $user. /home/$user/www
 	chgrp -R apache /home/$user/www
 	
-
-
 	##Database setup
 	dbname="$user"_db
 	mysql -u root -proot -e "CREATE DATABASE IF NOT EXISTS $dbname;GRANT ALL PRIVILEGES ON $dbname.* TO $user@localhost IDENTIFIED BY '$pass';FLUSH PRIVILEGES;"
 	;;
+	
+	cp $WSM/template-apache-2.4.conf /etc/http/conf.d/$user.conf
+	sed 's/user/'$user'/g' $WSM/template-apache-2.4.conf > /etc/httpd/conf.d/$user.tmp1
+	sed 's/domain/'$domain'/g' /etc/httpd/conf.d/$user.tmp1 > /etc/httpd/conf.d/$user.tmp2
+	sed 's/mail/'$mail'/g' /etc/httpd/conf.d/$user.tmp2 > /etc/httpd/conf.d/$user.conf
+
+	rm /etc/httpd/conf.d/$user.tmp* -f
+
+	systemctl reload httpd
 	
 	2)
 	
@@ -56,64 +65,15 @@ case $choice in
 	
 
 	4)
-	echo "Username: "
+	echo "Username of the user you want to delete:"
 	read user
-	echo "Domain name: "
-	read domain	
-	systemctl stop httpd
-	
-	./letsencrypt-auto certonly --standalone -d $domain
-
-	echo 'SSLPassPhraseDialog  builtin
-SSLSessionCache         shmcb:/var/cache/mod_ssl/scache(512000)
-SSLSessionCacheTimeout  300
-SSLMutex default
-
-SSLRandomSeed startup file:/dev/urandom  256
-SSLRandomSeed connect builtin
-
-SSLCryptoDevice builtin
-
-<VirtualHost *:443>
-
-SSLEngine on
-
-DocumentRoot "/home/$user/www"
-ServerName $domain:443
-
-ErrorLog logs/ssl_error_log
-TransferLog logs/ssl_access_log
-LogLevel warn
-
-SSLProtocol all -SSLv2 -SSLv3
-
-SSLCipherSuite "HIGH:!aNULL:!eNULL:!kECDH:!aDH:!RC4:!3DES:!CAMELLIA:!MD5:!PSK:!SRP:!KRB5:@STRENGTH"
-
-SSLCertificateFile /etc/letsencrypt/live/$domain/cert.pem
-SSLCertificateChainFile /etc/letsencrypt/live/$domain/chain.pem
-SSLCertificateKeyFile /etc/letsencrypt/live/$domain/privkey.pem
-
-SSLVerifyClient none
-SSLVerifyDepth  1
-
-<Files ~ "\.(cgi|shtml|phtml|php3?)$">
-    SSLOptions +StdEnvVars
-</Files>
-<Directory "/var/www/cgi-bin">
-    SSLOptions +StdEnvVars
-</Directory>
-<Directory />
-Allowoverride All
-</Directory>
-
-CustomLog logs/ssl_request_log \
-          "%t %h %{SSL_PROTOCOL}x %{SSL_CIPHER}x \"%r\" %b"
-
-</VirtualHost>' > /etc/httpd/conf.d/$user_ssl.conf
-
-	systemctl start httpd
+	userdel $user
+	dbname="$user"_db
+	rm -rvf /home/$user
+	rm /etc/httpd/conf.d/$user.conf
+	mysql -u root -proot -e "DROP USER $user@localhost;DROP DATABASE $dbname;"
 	;;
-
+	
 	5)
 	echo "Username of the user you want to delete:"
 	read user
